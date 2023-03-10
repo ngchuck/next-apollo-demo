117e629f4e3a8b7717a00d38ac3d257bbe07379c
ngchuck <deepc.au@gmail.com>
2023-03-10 17:16:35 +0000

    Update basic snapshot

--- client/src/__tests__/UserCard.test.tsx
+++ client/src/__tests__/UserCard.test.tsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react'
 import UserCard from "@/components/UserCard";
 import { MOCK_USER } from "@/mocks/user";
 
-
 it('renders UserCard unchanged', () => {
   const { container } = render(<UserCard user={MOCK_USER}/>)
   expect(container).toMatchSnapshot()
--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -1,19 +1,13 @@
 import {User} from '@/types/user';
 import styles from './UserCard.module.css'
 
-const UserCard = ({user}: { user: User }) => {
-  if (!user) {
-    return (<p> User has no data</p>)
-  }
-
-  return (
-    <div className={styles.card} key={user.email}>
-      <h3>{user.firstName} {user.lastName} - {user.age}</h3>
-      <p>{user.address.streetName}</p>
-      <p>{user.email}</p>
-      <p>{user.phone}</p>
-      <img src={user.avatar}/>
-    </div>
-  )
-}
+const UserCard = ({user}: { user: User }) => (
+  <div className={styles.card} key={user.email}>
+    <h3>{user.firstName} {user.lastName} - {user.age}</h3>
+    <p>{user.address.streetName}</p>
+    <p>{user.email}</p>
+    <p>{user.phone}</p>
+    <img src={user.avatar}/>
+  </div>
+);
 export default UserCard;
